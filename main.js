edbffecbdbe1462b394e69bc512c648feb55ac82
Daniel Juarez <juarezdaniel@gmail.com>
2022-01-18 19:53:19 -0500

    image and text framework

---
+++ main.js
@@ -0,0 +1,7 @@
+let swapButton = document.querySelector('button');
+let image = document.querySelector('img');
+
+swapButton.addEventListener('click', () => {
+    image.src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.JTjpuGJTh3wwKKKG2NEvvwHaFj%26pid%3DApi&f=1';
+})
+
